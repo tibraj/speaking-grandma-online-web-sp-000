44766158e423d9dccf7f59381379314cb60443f0
Taulant Ibraj <tibraj94@gmail.com>
2020-03-27 15:42:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
 end
 end
 # Whatever you say to grandma, she should respond with
